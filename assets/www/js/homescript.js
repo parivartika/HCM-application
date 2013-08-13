e88342ffb1037489ab0888958b318ccdef964d07
parivartika <parivartika@yahoo.com>
2013-08-13 21:39:22 +0530

    new homescript

--- assets/www/js/homescript.js
+++ assets/www/js/homescript.js
@@ -11,7 +11,7 @@ function onDeviceReady() {
 }
 
 /*function readydb(){
- 
+    
 }*/
 
 document.addEventListener("backbutton", function(e)
@@ -105,7 +105,7 @@ function onConfirm(buttonIndex) {
 }
 
 function checkConnection() {
-    var networkState = navigator.connection.type;
+    var networkState = navigator.network.connection.type;
     var states = {};
     states[Connection.UNKNOWN]  = 'Unknown connection';
     states[Connection.ETHERNET] = 'Ethernet connection';
@@ -119,16 +119,170 @@ function checkConnection() {
     alert("deivce is offline you need to connect to register");
 }
     else{
-    	if(localStorage.getItem("installation") == 'COMPLETE'){
-    		alert("if fucntion called");
-    		register_push_notification();
-    	}
-    	else{
-    		push_notification();
-    		}
+    	onOnline();
+    }
+}
+
+
+function onOffline(){
+	alert("deivce is offline you need to connect to register");
+}
+
+function onOnline(){
+ 
+ alert("devie is online");
+   // $("#app-status-ul").append('<li>deviceready event received</li>');
+    //alert("deviceready event received");
+	 document.addEventListener("backbutton", function(e)
+	{
+    	$("#app-status-ul").append('<li>backbutton event received</li>');
+			
+			if( $("#home").length > 0)
+		{
+			window.localStorage["appexit"]= 'COMPLETE';
+			// call this to get a new token each time. don't call it to reuse existing token.
+			//pushNotification.unregister(successHandler, errorHandler);
+			//e.preventDefault();
+			navigator.app.exitApp();
+		}
+		else
+		{
+			navigator.app.backHistory();
+		}
+	}, false);
+
+	try 
+	{ 
+    	pushNotification = window.plugins.pushNotification;
+    	if (device.platform == 'android' || device.platform == 'Android') {
+			//$("#app-status-ul").append('<li>registering android</li>');
+			alert("registering android");
+			pushNotification.register(successHandler, errorHandler, {"senderID":"198872155807","ecb":"onNotificationGCM"});		// required!
+		} else {
+			$("#app-status-ul").append('<li>registering iOS</li>');
+        	pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
     	}
     }
+	catch(err) 
+	{ 
+		txt="There was an error on this page.\n\n"; 
+		txt+="Error description: " + err.message + "\n\n"; 
+		alert(txt); 
+	} 
+}
 
+// handle APNS notifications for iOS
+function onNotificationAPN(e) {
+    if (e.alert) {
+         $("#app-status-ul").append('<li>push-notification: ' + e.alert + '</li>');
+         navigator.notification.alert(e.alert);
+    }
+        
+    if (e.sound) {
+        var snd = new Media(e.sound);
+        snd.play();
+    }
+    
+    if (e.badge) {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+    }
+}
+
+// handle GCM notifications for Android
+function onNotificationGCM(e) {
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+    
+    switch( e.event )
+    {
+        case 'registered':
+		if ( e.regid.length > 0 )
+		{
+			//$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+			
+			alert("REGISTERED -> REGID:" + e.regid);
+			// Your GCM push server needs to know the regID before it can push to this device
+			// here is where you might want to send it the regID for later use.
+			window.localStorage["regID"]= e.regid;
+			
+			 $.ajax({
+           url: 'http://192.168.43.188/gcm_server_php/register.php', //This is the current doc
+           type: "POST",
+           data: {name: "abhi", email: "notification@eclipseregid11111", regId: e.regid},
+           success: function(data){
+           console.log(data);
+           }
+           
+           });
+			
+			console.log("regID = " + e.regID);
+			
+			alert("regidtered");
+			
+			 
+
+			
+		}
+        break;
+        
+        case 'message':
+        	// if this flag is set, this notification happened while we were in the foreground.
+        	// you might want to play a sound to get the user's attention, throw up a dialog, etc.
+        	if (e.foreground)
+        	{
+				//$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+				
+				// if the notification contains a soundname, play it.
+				var my_media = new Media("/android_asset/www/"+e.soundname);
+				my_media.play();
+			}
+			else
+			{	// otherwise we were launched because the user touched a notification in the notification tray.
+				if (e.coldstart)
+					//$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+					alert("cold strat");
+					else
+				$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+			}
+				
+			//$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+			alert("MESSAGE -> MSG:"+ e.payload.message);
+			//$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+        break;
+        
+        case 'error':
+			//$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+        	alert(e.msg);
+        break;
+        
+        default:
+			//$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+        break;
+    }
+}
+
+
+
+function tokenHandler (result) {
+    //$("#app-status-ul").append('<li>token: '+ result +'</li>');
+    // Your iOS push server needs to know the token before it can push to this device
+    // here is where you might want to send it the token for later use.
+}
+
+function successHandler (result) {
+    //$("#app-status-ul").append('<li>success:'+ result +'</li>');
+    alert('result = '+result);
+}
+
+function errorHandler (error) {
+    //$("#app-status-ul").append('<li>error:'+ error +'</li>');
+    alert('error = '+error);
+}
+
+/*function change_page(){
+	var urlHome="index.html";
+    location=urlHome;
+}
+*/
 
 function confirm_notification(){
 	//alert("confirm notif function called");
@@ -211,7 +365,7 @@ function exitapp(){
 	}
 }
 
-function push_notification(){
+/*function push_notification(){
 	
    // $("#app-status-ul").append('<li>deviceready event received</li>');
 	//alert("devicerrady event received..");
@@ -230,7 +384,7 @@ function push_notification(){
 		            'Yes,No'          // buttonLabels
 		        );
 			
-	        //e.preventDefault();*/
+	        //e.preventDefault();
 	        navigator.app.exitApp();
 	    }
 	    else {
@@ -240,10 +394,10 @@ function push_notification(){
 
 	try 
 	{ 
-    	pushNotification = window.plugins.pushNotification;
+		pushNotification = window.plugins.pushNotification;
     	if (device.platform == 'android' || device.platform == 'Android') {
 			//$("#app-status-ul").append('<li>registering android</li>');
-			//alert("registering android..");
+			alert("registering android..");
         	pushNotification.register(successHandler, errorHandler, {"senderID":"198872155807","ecb":"onNotificationGCM"});		// required!
 		} else {
 			$("#app-status-ul").append('<li>registering iOS</li>');
@@ -292,7 +446,7 @@ function onNotificationGCM(e) {
 			window.localStorage["regID"]= e.regid;
 			
 			 $.ajax({
-           url: 'http://192.168.43.188/gcm_server_php/register.php', //This is the current doc
+           url: 'http://10.0.2.2/gcm_server_php/register.php', //This is the current doc
            type: "POST",
            data: {name: "abhi", email: "helloworld@eclipseregid11111", regId: e.regid},
            success: function(data){
@@ -378,7 +532,7 @@ function errorHandler (error) {
     alert('error = '+error);
 } 
 
-
+*/
 
 function errorhandler(e){
     alert(e.message);
